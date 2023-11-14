4afd7955fb777a498f3803574b1dd1bb54ebe1b4
Zibec <primofmf@gmail.com>
2023-11-14 10:14:50 -0300

    Menu Corrigido

--- Menu.py
+++ Menu.py
@@ -1,50 +1,7 @@
-livros = {}
-
-def adicionar_livro():
-    livro_id = input("Digite o ID do livro: ")
-    titulo = input("Digite o título do livro: ")
-    autor = input("Digite o nome do autor: ")
-    ano = input("Digite o ano de publicação: ")
-
-    livros[livro_id] = {
-        'titulo': titulo,
-        'autor': autor,
-        'ano': ano
-    }
-    print(f'O livro "{titulo}" foi adicionado com sucesso!')
-
-def visualizar_livros():
-    print("Lista de Livros:")
-    for livro_id, livro_info in livros.items():
-        print(f"ID: {livro_id}, Título: {livro_info['titulo']}, Autor: {livro_info['autor']}, Ano: {livro_info['ano']}")
-
-def atualizar_livro():
-    livro_id = input("Digite o ID do livro que deseja atualizar: ")
-    if livro_id in livros:
-        print(f"Atualizando livro {livros[livro_id]['titulo']}:")
-        novo_titulo = input("Novo título (deixe em branco para manter o atual): ")
-        novo_autor = input("Novo autor (deixe em branco para manter o atual): ")
-        novo_ano = input("Novo ano de publicação (deixe em branco para manter o atual): ")
-
-        if novo_titulo:
-            livros[livro_id]['titulo'] = novo_titulo
-        if novo_autor:
-            livros[livro_id]['autor'] = novo_autor
-        if novo_ano:
-            livros[livro_id]['ano'] = novo_ano
-
-        print("Livro atualizado com sucesso!")
-    else:
-        print("Livro não encontrado.")
-
-def excluir_livro():
-    livro_id = input("Digite o ID do livro que deseja excluir: ")
-    if livro_id in livros:
-        titulo = livros[livro_id]['titulo']
-        del livros[livro_id]
-        print(f'O livro "{titulo}" foi excluído com sucesso!')
-    else:
-        print("Livro não encontrado.")
+from Pedro import adicionar_livro
+from Pedro import visualizar_livros
+from Pedro import atualizar_livro
+from Pedro import excluir_livro
 
 while True:
     print("\nMenu:")
