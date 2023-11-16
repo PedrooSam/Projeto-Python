4260a42fd46d9f26742979f827fe17d60f3db3f7
Pedro Sampaio <pssa@cesar.school>
2023-11-16 17:37:23 -0300

    Alterações no cadastro de novos livros
    alterei a forma que os livros ficam armazenados no arquivo de texto e deixei o terminal mais bonito utilizando as bibliotecas os e time

--- Cadastro_de_livros.py
+++ Cadastro_de_livros.py
@@ -1,19 +1,35 @@
-livro=[]
+import os
+from time import sleep
 
-titulo=input("Título: ").title()
-autor=input("Autor: ").title()
-categoria=input("Categoria: ").title()
-preco=float(input("Preço: "))
+def adicionar_livro():
+    os.system("cls")
+    print("Carregando...")
+    sleep(1.5)
+    os.system("cls")
 
-livro.append(titulo)
-livro.append(autor)
-livro.append(categoria)
-livro.append(preco)
+    print("-----ADICIONAR LIVRO-----\n")
 
-livros=open("livros.txt", "a")
-livros.write(livro[0]+"\n")
-livros.write(livro[1]+"\n")
-livros.write(livro[2]+"\n")
-livros.write(str(livro[3])+"\n")
-livros.close()
+    livro=[]
 
+    titulo=input("Título: ").title()
+    autor=input("Autor: ").title()
+    categoria=input("Categoria: ").title()
+    preco=float(input("Preço: "))
+
+    livro.append(titulo)
+    livro.append(autor)
+    livro.append(categoria)
+    livro.append(preco)
+
+    livros=open("livros.txt", "a", encoding='utf-8')
+    livros.write(livro[0]+";")
+    livros.write(livro[1]+";")
+    livros.write(livro[2]+";")
+    livros.write(str(livro[3])+"\n")
+
+    livros.close()
+
+    print("\nLivro Adicionado!")
+    sleep(1.5)
+
+adicionar_livro()
