41945294e8df3b5aa35b8b0fb0654fb75e84260b
Zibec <primofmf@gmail.com>
2023-11-19 17:37:38 -0300

    Adição view

--- Menu.py
+++ Menu.py
@@ -1,10 +1,9 @@
 import Cadastro_de_livros
 import Atualizar_livros
+import Visualizar_livros
 import Funcoes
-import os
 
 while True:
-    os.system('cls') 
 
     print("\nMenu:")
     print("1. Adicionar Livro")
@@ -18,7 +17,22 @@ while True:
     if escolha == "1":
         Cadastro_de_livros.adicionar_livro()
     elif escolha == "2":
-        Funcoes.visualizar_livros()
+        print("\nEscolha o tipo de categoria:")
+        print("Geral [0]")
+        print("Categoria [1]")
+        print("Autor [2]")
+        
+        tipo_view = input("Digite o número da opção desejada: ")
+
+        if tipo_view == '0':
+            print(Visualizar_livros.visualizacao_geral())
+        elif tipo_view == '1':
+            categoria = input("Digite a categoria desejada: ")
+            print(Visualizar_livros.visualizacao_por_categoria(categoria))
+        elif tipo_view == '2':
+            autor = input("Digite o autor desejado: ")
+            print(Visualizar_livros.visualizacao_por_autor(autor))
+    
     elif escolha == "3":
         Atualizar_livros.atualizar_livro()
     elif escolha == "4":
