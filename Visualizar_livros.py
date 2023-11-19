c92baba9f07937942765cad5a614e3f26a319a3d
França <143142740+FelipeARFranca@users.noreply.github.com>
2023-11-19 16:38:33 -0300

    Visualizar_livros.py

---
+++ Visualizar_livros.py
@@ -0,0 +1,129 @@
+def visualizacao_geral ():
+    arquivo = open('livros.txt', 'r', encoding='utf-8')
+
+    info = arquivo.readlines()
+
+    arquivo.close()
+
+    livros = []
+    quant_livros = 0
+    total_gasto = 0
+
+    for linha in range (len(info)):
+        livro = info[linha]
+        livro = livro.split(';')
+
+        valor = float(livro[3])
+
+        livro = f'''_______________________________________________________________________________
+
+Livro: {livro[0]}
+
+Autor: {livro[1]}
+Categoria: {livro[2]}
+Valor: R${livro[3]}'''
+
+        quant_livros += 1
+        total_gasto += valor
+        livros.append(livro)
+    
+    print (f'''_______________________________________________________________________________
+
+Total de livros cadastrados: {quant_livros}    Total gasto: R${total_gasto}''')
+
+    for c in range (len(livros)):
+        print (livros[c])
+    return ''
+
+
+#=======================================================================================================================================
+
+
+def visualizacao_por_categoria (categoria):
+        arquivo = open('livros.txt', 'r', encoding='utf-8')
+
+        info = arquivo.readlines()
+
+        arquivo.close()
+
+        livros = []
+        quant_livros = 0
+        total_gasto = 0
+
+        for linha in range (len(info)):
+            livro = info[linha]
+            livro = livro.split(';')
+
+            if livro[2] == categoria:
+                valor = float(livro[3])
+
+                livro = f'''_______________________________________________________________________________
+
+Livro: {livro[0]}
+
+Autor: {livro[1]}
+Categoria: {livro[2]}
+Valor: R${livro[3]}'''
+                
+                quant_livros += 1
+                total_gasto += valor
+                livros.append(livro)
+
+        if livros != []:
+
+            print (f'''_______________________________________________________________________________
+
+Nº de livros encontrados na categoria: {quant_livros}    Total gasto na categoria: R${total_gasto}''')
+
+            for c in range (len(livros)):
+                print (livros[c])
+        else:
+            print ('Categoria não encontrada!')
+        return ''
+      
+
+#=======================================================================================================================================
+
+
+def visualizacao_por_autor (autor):
+        arquivo = open('livros.txt', 'r', encoding='utf-8')
+
+        info = arquivo.readlines()
+
+        arquivo.close()
+
+        livros = []
+        quant_livros = 0
+        total_gasto = 0
+
+        for linha in range (len(info)):
+            livro = info[linha]
+            livro = livro.split(';')
+
+            if livro[1] == autor:
+                valor = float(livro[3])
+
+                livro = f'''_______________________________________________________________________________
+
+Livro: {livro[0]}
+
+Autor: {livro[1]}
+Categoria: {livro[2]}
+Valor: R${livro[3]}'''
+                
+
+                quant_livros += 1
+                total_gasto += valor
+                livros.append(livro)
+
+        if livros != []:
+
+            print (f'''_______________________________________________________________________________
+
+Nº de livros encontrados do autor: {quant_livros}    Total gasto com o autor: R${total_gasto}''')
+
+            for c in range (len(livros)):
+                print (livros[c])
+        else:
+            print ('Autor não encontrado!')
+        return ''
