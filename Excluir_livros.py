857c53521d14c74c06d2a26d03e5b3897a7db963
luiscoast <lacc2@cesar.school>
2023-11-20 20:51:57 -0300

    Create Excluir_livros.py

---
+++ Excluir_livros.py
@@ -0,0 +1,19 @@
+def excluir_livro():
+    arquivo = open('livros.txt', 'r', encoding='utf-8')
+    info = arquivo.readlines()
+    arquivo.close()
+
+    for linha in range(len(info)):
+        livro = info[linha].split(';')
+        print(f"({linha}) {livro[0]}")
+
+    livro_excluido = int(input("Digite o número do livro que você gostaria de excluir: "))
+
+    if 0 <= livro_excluido < len(info):
+        info.pop(livro_excluido)
+
+        arquivo = open('livros.txt', 'w', encoding='utf-8')
+        arquivo.writelines(info)
+        arquivo.close()
+    else:
+        print("Número inválido. Nenhum livro foi excluído.")
