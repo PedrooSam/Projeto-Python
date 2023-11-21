cb2d6ea5f20bcc2562032aeb4a871af3666b2584
luiscoast <lacc2@cesar.school>
2023-11-21 19:24:13 -0300

    Update Cadastro_de_livros.py

--- Cadastro_de_livros.py
+++ Cadastro_de_livros.py
@@ -16,62 +16,62 @@ def adicionar_livro():
     print("1. Fantasia\n2. Ficção Científica\n3. Distopia\n4. Ação e Aventura\n5. Ficção Policial\n6. Horror\n7. Thriller e Suspense\n8. Ficção Histórica\n9. Romance\n10. Novela\n11. Ficção Feminina\n12. LGBT\n13. Ficção Comtemporânea\n14. Realismo Mágico\n15. Graphic Novel\n16. Conto\n17. Jovem Adulto\n18. Novo Adulto\n19. Infantil")
     while True:
         categoria = input("Categoria (número entre 1 e 19): ")
-        if categoria.isdigit() and 1 <= int(categoria) <= 19:
-            if categoria == 1:
+        if categoria.isdigit():
+            if categoria == '1':
                 livro.append("FANTASIA")
                 break
-            if categoria == 2:
+            if categoria == '2':
                 livro.append("FICÇÃO CIENTÍFICA")
                 break
-            if categoria == 3:
+            if categoria == '3':
                 livro.append("DISTOPIA")
                 break
-            if categoria == 4:
+            if categoria == '4':
                 livro.append("AÇÃO E AVENTURA")
                 break
-            if categoria == 5:
+            if categoria == '5':
                 livro.append("FICÇÃO POLICIAL")
                 break
-            if categoria == 6:
+            if categoria == '6':
                 livro.append("HORROR")
                 break
-            if categoria == 7:
+            if categoria == '7':
                 livro.append("THRILLER E SUSPENSE")
                 break
-            if categoria == 8:
+            if categoria == '8':
                 livro.append("FICÇÃO HISTÓRICA")
                 break
-            if categoria == 9:
+            if categoria == '9':
                 livro.append("ROMANCE")
                 break
-            if categoria == 10:
+            if categoria == '10':
                 livro.append("NOVELA")
                 break
-            if categoria == 11:
+            if categoria == '11':
                 livro.append("FICÇÃO FEMININA")
                 break
-            if categoria == 12:
+            if categoria == '12':
                 livro.append("LGBT")
                 break
-            if categoria == 13:
+            if categoria == '13':
                 livro.append("FICÇÃO CONTEMPORÂNEA")
                 break
-            if categoria == 14:
+            if categoria == '14':
                 livro.append("REALISMO MÁGICO")
                 break
-            if categoria == 15:
+            if categoria == '15':
                 livro.append("GRAPHIC NOVEL")
                 break
-            if categoria == 16:
+            if categoria == '16':
                 livro.append("CONTO")
                 break
-            if categoria == 17:
+            if categoria == '17':
                 livro.append("JOVEM ADULTO")
                 break
-            if categoria == 18:
+            if categoria == '18':
                 livro.append("NOVO ADULTO")
                 break
-            if categoria == 19:
+            if categoria == '19':
                 livro.append("INFANTIL")
                 break
         else:
