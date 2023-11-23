50b368af70d8f2cad1fcaa6df19d12700b720c13
França <143142740+FelipeARFranca@users.noreply.github.com>
2023-11-22 21:25:54 -0300

    Update Cadastro_de_livros.py

--- Cadastro_de_livros.py
+++ Cadastro_de_livros.py
@@ -1,82 +1,112 @@
-import os
+import Tratamento_de_Erros
 from time import sleep
 
 def adicionar_livro():
-    os.system("cls")
-    print("Carregando...")
-    sleep(1.5)
-    os.system("cls")
-
     print("-----ADICIONAR LIVRO-----\n")
 
     livro=[]
 
     livro.append(input("Título: ").title())
     livro.append(input("Autor: ").title())
+
     print("1. Fantasia\n2. Ficção Científica\n3. Distopia\n4. Ação e Aventura\n5. Ficção Policial\n6. Horror\n7. Thriller e Suspense\n8. Ficção Histórica\n9. Romance\n10. Novela\n11. Ficção Feminina\n12. LGBT\n13. Ficção Comtemporânea\n14. Realismo Mágico\n15. Graphic Novel\n16. Conto\n17. Jovem Adulto\n18. Novo Adulto\n19. Infantil")
+    
     while True:
+
         categoria = input("Categoria (número entre 1 e 19): ")
+
         if categoria.isdigit() and 1 <= int(categoria) <= 19:
+
             if categoria == '1':
                 livro.append("FANTASIA")
                 break
+
             if categoria == '2':
                 livro.append("FICÇÃO CIENTÍFICA")
                 break
+
             if categoria == '3':
                 livro.append("DISTOPIA")
                 break
+
             if categoria == '4':
                 livro.append("AÇÃO E AVENTURA")
                 break
+
             if categoria == '5':
                 livro.append("FICÇÃO POLICIAL")
                 break
+
             if categoria == '6':
                 livro.append("HORROR")
                 break
+
             if categoria == '7':
                 livro.append("THRILLER E SUSPENSE")
                 break
+
             if categoria == '8':
                 livro.append("FICÇÃO HISTÓRICA")
                 break
+
             if categoria == '9':
                 livro.append("ROMANCE")
                 break
+
             if categoria == '10':
                 livro.append("NOVELA")
                 break
+
             if categoria == '11':
                 livro.append("FICÇÃO FEMININA")
                 break
+
             if categoria == '12':
                 livro.append("LGBT")
                 break
+
             if categoria == '13':
                 livro.append("FICÇÃO CONTEMPORÂNEA")
                 break
+
             if categoria == '14':
                 livro.append("REALISMO MÁGICO")
                 break
+
             if categoria == '15':
                 livro.append("GRAPHIC NOVEL")
                 break
+
             if categoria == '16':
                 livro.append("CONTO")
                 break
+
             if categoria == '17':
                 livro.append("JOVEM ADULTO")
                 break
+
             if categoria == '18':
                 livro.append("NOVO ADULTO")
                 break
+
             if categoria == '19':
                 livro.append("INFANTIL")
                 break
+
         else:
             print("Entrada inválida. Tente novamente.")
-    livro.append(float(input("Preço: ")))
+    
+    while True:
+    
+        preco = input('Preço: ')
+
+        if Tratamento_de_Erros.numeric_confirm(preco):
+            livro.append(float(preco))
+            break
+
+        else:
+            print ('Insira um preço válido!')
+            continue
 
     livro[3]="{:.2f}".format(livro[3])
 
